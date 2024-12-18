479e09e7fe1910df92b15f0d25e8e27cb4bfe351
Sander-132 <70809337+Sander-132@users.noreply.github.com>
2024-12-18 15:46:32 +0100

    Server Changes
    Added some server functionality from Olivier receive and send data from the server

--- SchemerlampProbeersel.ino
+++ SchemerlampProbeersel.ino
@@ -1,16 +1,38 @@
-#include <Servo.h>
+#include <unistd.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h> //IIRC this could be used to track a hostname instead of a constantly changing IP address.
 #include <FastLED.h>  //Version 3.3.0 installed to not run into certain problems like last time.
 
+
 int RegistrateMovement(); //Registrating movement when someone enters the appartment, for the signal to the security.
 void TurnLampOn();
 void TurnLampOff();
 
+volatile int prevMovement = 0;
+volatile int teller = 0;
+
 #define LED_PIN D5 //De registrated pin of the lamp.
 #define LED 1
 CRGB led[LED]; //To be able to change the color of the lamp.
+
+#define I2C_SDL    D1 //klok lijn
+#define I2C_SDA    D2 // data lijn
+
+#ifndef STASSID
+#define STASSID "PiVanOli4"
+#define STAPSK "WachtwoordPiVanOli4"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+const uint16_t port = 8080;
+const char* host = "Server Pi van Oli4";
+IPAddress local_Ip(192,168,10,22); //wanneer de pi dhcp zou hebben zou dit niet nodig zijn en is de code helemaal identiek op beide wemossen
+IPAddress gateway(192,168,10,1); //Dit zal vast wel elke keer aangepast moeten worden
+IPAddress subnet(255,255,255,0);
+
+WiFiClient client;
 
 void setup()
 {
@@ -28,12 +50,89 @@ void setup()
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 500); //Maximum power drawage.
   FastLED.clear(); //Clear the remaining buffer.
   FastLED.show(); //Update the lamp with the current data.
+
+  //Server setup
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.config(local_Ip, gateway, subnet);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Verbonden met het netwerk!");
+
+  prevMovement = RegistrateMovement();
 }
+
 
 void loop() 
 {
-  RegistrateMovement();
-  TurnLampOn();
+  if(!client.connected()){
+    Serial.print("connecting to ");
+    Serial.print(host);
+    Serial.print(':');
+    Serial.println(port);
+    delay(500);
+    client.connect(gateway, 8080);  
+  }
+  
+  //Serial.println("sending data to server");
+  if (client.connected()) {
+    //Is er iets uit te lezen
+    if (client.available() > 0) {
+      String message = ""; // Buffer om het bericht op te slaan
+      // Lees alle beschikbare bytes
+      while (client.available()) {
+        char c = client.read(); // Lees een karakter
+        message += c;           // Voeg het toe aan de buffer
+      }
+
+    // Toon het volledige bericht
+    Serial.print("Ontvangen bericht: ");
+    Serial.println(message);
+    }
+    //is er iets om te versturen?
+    client.print("test");
+    delay(800);
+  }
+
+  MovementHandler();
+  ToggleLamp();   
+}
+
+void MovementHandler() // Afhandelen van bewegingsregistratie
+{
+  int newMovement = RegistrateMovement();
+  Serial.println(newMovement);
+
+  if(newMovement != prevMovement)
+  {
+    Serial.println("Someone is home!");
+    prevMovement = newMovement;
+  }
+  else
+    Serial.println("Nothing suspicious.");  
+}
+
+void ToggleLamp()
+{
+   if(teller == 0)
+    {
+      TurnLampOn();
+      teller = 1;
+    }
+    else
+    {
+      TurnLampOff();
+      teller = 0;
+    }  
+    delay(500);  
 }
 
 int RegistrateMovement()
@@ -41,7 +140,7 @@ int RegistrateMovement()
   Wire.beginTransmission(0x38); //PIR sensor is DI0 on address 38, so that's where we decide to transmit to.
   Wire.write(byte(0x00)); //Write the data of the very first bit on this address.
   Wire.endTransmission(); //End the data transmission.
-  Wire.requestFrom(0x38,1); //We want to request the byte from this address now, and do this once.
+  Wire.requestFrom(0x38,1); //We want to request one byte from this address. 
   int readValue = Wire.read(); //Read the current value that is being registered by the movement sensor. 
   return readValue;
 }
