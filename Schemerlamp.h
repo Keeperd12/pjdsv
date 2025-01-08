2fabc0687a21a33e4f8285e29f3018f333a003c1
Sander-132 <70809337+Sander-132@users.noreply.github.com>
2025-01-08 12:10:00 +0100

    Schemerlamp.h
    Aanmaken van de file met eerste data

---
+++ Schemerlamp.h
@@ -0,0 +1,14 @@
+#pragma once //Voorkomen van meervoudige inclusie van de header file
+
+#include "Client.h"
+
+class Schemerlamp : public Client //Overerven van de Client klasse
+{
+public:
+    Schemerlamp(int, int); //Constructor
+    ~Schemerlamp(); //Destructor
+
+//Functies nog toevoegen
+private:
+//Variabelen nog toevoegen
+};
