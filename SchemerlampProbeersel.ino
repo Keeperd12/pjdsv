c412a26e48b20972c4a1f0c71f8fb2224da52151
Sander-132 <70809337+Sander-132@users.noreply.github.com>
2025-01-16 12:44:07 +0100

    Update SchemerlampProbeersel.ino
    Small changes made + including UpdateHandler() function

--- SchemerlampProbeersel.ino
+++ SchemerlampProbeersel.ino
@@ -5,12 +5,15 @@
 #include <FastLED.h>  //Version 3.3.0 installed to not run into certain problems like last time.
 
 
+void UpdateHandler(); //Een functie die alleen naar de server schrijft bij statuswijzingen.
+void MovementHandler(); //Afhandelen van bewegingsregistratie.
 int RegistrateMovement(); //Registrating movement when someone enters the appartment, for the signal to the security.
 void TurnLampOn();
 void TurnLampOff();
 
 volatile int prevMovement = 0;
-volatile int teller = 0;
+volatile int lampState = 0;
+volatile int prevLampState = 0;
 
 #define LED_PIN D5 //De registrated pin of the lamp.
 #define LED 1
@@ -20,16 +23,19 @@ CRGB led[LED]; //To be able to change the color of the lamp.
 #define I2C_SDA    D2 // data lijn
 
 #ifndef STASSID
-#define STASSID "PiVanOli4"
-#define STAPSK "WachtwoordPiVanOli4"
+//#define STASSID "PiVanOli4"
+#define STASSID "Ramon"
+//#define STAPSK "WachtwoordPiVanOli4"
+#define STAPSK "12345678"
 #endif
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
 const uint16_t port = 8080;
-const char* host = "Server Pi van Oli4";
-IPAddress local_Ip(192,168,10,22); //wanneer de pi dhcp zou hebben zou dit niet nodig zijn en is de code helemaal identiek op beide wemossen
-IPAddress gateway(192,168,10,1); //Dit zal vast wel elke keer aangepast moeten worden
+//const char* host = "Server Pi van Oli4";
+const char *host = "Ramon";
+IPAddress local_Ip(192,168,10,5); //wanneer de pi dhcp zou hebben zou dit niet nodig zijn en is de code helemaal identiek op beide wemossen
+IPAddress gateway(192,168,10,1); 
 IPAddress subnet(255,255,255,0);
 
 WiFiClient client;
@@ -80,7 +86,7 @@ void loop()
     Serial.println(port);
     delay(500);
     client.connect(gateway, 8080);  
-  }
+  } 
   
   //Serial.println("sending data to server");
   if (client.connected()) {
@@ -90,7 +96,7 @@ void loop()
       // Lees alle beschikbare bytes
       while (client.available()) {
         char c = client.read(); // Lees een karakter
-        message += c;           // Voeg het toe aan de buffer
+          message += c;           // Voeg het toe aan de buffer
       }
 
     // Toon het volledige bericht
@@ -98,22 +104,62 @@ void loop()
     Serial.println(message);
     }
     //is er iets om te versturen?
-    client.print("test");
+    client.print("Schemerlamp");
     delay(800);
+
+    //MovementHandler(); //Volgens is MovementHandler volledig in functionaliteit onderdanig aan UpdateHandler nu.
+    ToggleLamp(); 
+    UpdateHandler();  
+  }
+  //delay(100); 
+}
+
+void UpdateHandler()
+{
+  // Checking the movement sensor, only a status change should lead to a message to the server.
+  if (prevMovement != RegistrateMovement()) 
+  {
+    if (RegistrateMovement() == 255) 
+    { 
+      client.print("Movement state changed. Someone is home!");
+      Serial.println("Data has been sent, movement has been detected, someone is home.");
+    } 
+    else 
+    {
+      client.print("Movement state changed. Nobody's home anymore");
+      Serial.println("Data has been sent, no movement is being traced right now.");
+    }
+    prevMovement = RegistrateMovement(); //De verandering opslaan als de nieuwe waarde om mee te vergelijken.
   }
 
-  MovementHandler();
-  ToggleLamp();   
+  // Checking the lamp state, only a status change from the lamp should lead to a message to the server.
+  if (lampState != prevLampState) 
+  {
+    ToggleLamp();
+    
+    if (lampState == 1) 
+    {
+      client.print("Lamp is turned on");
+      Serial.println("Data has been sent, lamp is turned on.");
+    } 
+    else if(lampState == 0)
+    {
+      client.print("Lamp is turned off");
+      Serial.println("Data has been sent, lamp is turned off.");
+    }
+    Serial.println(lampState);
+    prevLampState = lampState; //De verandering opslaan als de nieuwe waarde om mee te vergelijken.
+  }
 }
 
 void MovementHandler() // Afhandelen van bewegingsregistratie
 {
   int newMovement = RegistrateMovement();
-  Serial.println(newMovement);
+  //Serial.println(newMovement);
 
   if(newMovement != prevMovement)
   {
-    Serial.println("Someone is home!");
+    Serial.println("Movement state changed. Someone is home!");
     prevMovement = newMovement;
   }
   else
@@ -122,22 +168,22 @@ void MovementHandler() // Afhandelen van bewegingsregistratie
 
 void ToggleLamp()
 {
-   if(teller == 0)
+   if(lampState == 0)
     {
       TurnLampOn();
-      teller = 1;
+      lampState = 1;
     }
-    else
+    else if( lampState == 1)
     {
       TurnLampOff();
-      teller = 0;
+      lampState = 0;
     }  
     delay(500);  
 }
 
 int RegistrateMovement()
 {
-  Wire.beginTransmission(0x38); //PIR sensor is DI0 on address 38, so that's where we decide to transmit to.
+  Wire.beginTransmission(0x38); //PIR sensor is DI0 on address 38, so that's where we decide to transmit to, via I2C.
   Wire.write(byte(0x00)); //Write the data of the very first bit on this address.
   Wire.endTransmission(); //End the data transmission.
   Wire.requestFrom(0x38,1); //We want to request one byte from this address. 
