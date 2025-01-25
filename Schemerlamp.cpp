e96a9f6857472398fa67d7a544c0e29b7093ec38
Sander-132 <70809337+Sander-132@users.noreply.github.com>
2025-01-25 12:31:50 +0100

    Update Schemerlamp.cpp
    Code oppoetsen en alleen aan Mary een bericht sturen voor geen beweging als ze zelf niet thuis is.

--- Schemerlamp.cpp
+++ Schemerlamp.cpp
@@ -8,18 +8,19 @@
 
 Schemerlamp::Schemerlamp(int fd, int Type, Server *s) : Client(fd, Type, s) {}
 Schemerlamp::~Schemerlamp() {}
+
 void Schemerlamp::Update(char *bericht)
 {
     std::cout << "De lengte van het bericht is: " << strlen(bericht) << std::endl;
     // bericht is van de schemerlamp zelf!
     if (strlen(bericht) == 3)
     {
-
         this->beweging = atoi(bericht);
         moetIkIetsDoen(bericht);
     }
     //dit is een update van de potmeter van de muur
-    if(strlen(bericht) ==8){
+    if(strlen(bericht) ==8)
+    {
         char temp[20];
         strcpy(temp, bericht);
         //std::string TempWaardeLed(bericht);
@@ -30,27 +31,32 @@ void Schemerlamp::Update(char *bericht)
         server->stuurBericht(GeefFD(), temp);
     }
 }
+
 char *Schemerlamp::GeefData()
 {
     return nullptr;
 }
+
 void Schemerlamp::moetIkIetsDoen(char *bericht)
 {
     // kijken of er beweging is
     if (beweging == 255)
     {
-        std::cout << "ER is beweging, signaal moet nog doorgegeven worden aan mary" << std::endl;
+        std::cout << "Er is beweging, signaal moet nog doorgegeven worden aan de bewaker als dit niet Mary is." << std::endl;
 
         for (auto it = server->GeefPointerMap().begin(); it != server->GeefPointerMap().end(); it++)
         {
             if (it->second->GeefType() == 5)
             {
                 Client *client = it->second;
-                Mary *mary = dynamic_cast<Mary* >(client);
-                if(mary->isMaryThuis() == 0){
-                    for(auto its = server->GeefPointerMap().begin(); its != server->GeefPointerMap().end(); its++){
-                        if(its->second->GeefType() == 6){
-                            server->stuurBericht(its->first, "Inbraak! beweging in mary haar appartement!\n" );
+                Mary *mary = dynamic_cast<Mary*>(client);
+                if(mary->isMaryThuis() == 0)
+                {
+                    for(auto its = server->GeefPointerMap().begin(); its != server->GeefPointerMap().end(); its++)
+                    {
+                        if(its->second->GeefType() == 6)
+                        {
+                            server->stuurBericht(its->first, "Inbraak! Beweging in Mary haar appartement!\n" );
                         }
                     }
                 }
@@ -60,14 +66,18 @@ void Schemerlamp::moetIkIetsDoen(char *bericht)
     }
     if (beweging == 254)
     {
-        std::cout << "Er is geen beweging meer" << std::endl;
-        std::cout << "ER is beweging, signaal moet nog doorgegeven worden aan mary" << std::endl;
+        std::cout << "Er is geen beweging meer, signaal moet nog doorgegeven worden aan Mary" << std::endl;
 
         for (auto it = server->GeefPointerMap().begin(); it != server->GeefPointerMap().end(); it++)
         {
             if (it->second->GeefType() == 5)
             {
-                server->stuurBericht(it->first, "Er is geen beweging meer!");
+                Client *client = it->second;
+                Mary *mary = dynamic_cast<Mary*>(client);
+                if(mary->isMaryThuis() == 0)
+                {
+                    server->stuurBericht(it->first, "Er is bij je ingebroken! Er is nu geen beweging meer!");
+                }
             }
         }
     }
