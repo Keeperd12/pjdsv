0c2dea7c712a6bfda30f7423d6dc0c0eea410df2
Sander-132 <70809337+Sander-132@users.noreply.github.com>
2025-01-27 12:25:25 +0100

    Update Schemerlamp.cpp
    Opschonen + comments toegevoegd

--- Schemerlamp.cpp
+++ Schemerlamp.cpp
@@ -6,77 +6,71 @@
 #include "Server.h"
 #include <bitset>
 
-Schemerlamp::Schemerlamp(int fd, int Type, Server *s) : Client(fd, Type, s) {}
-Schemerlamp::~Schemerlamp() {}
+Schemerlamp::Schemerlamp(int fd, int Type, Server *s) : Client(fd, Type, s) {} //Constructor voor Schemerlamp object via het aanroepen van de base class constructor
+Schemerlamp::~Schemerlamp() {} //Destructor
 
-void Schemerlamp::Update(char *bericht)
+void Schemerlamp::Update(char *bericht) //Verwerken van ontvangen berichten 
 {
     std::cout << "De lengte van het bericht is: " << strlen(bericht) << std::endl;
-    // bericht is van de schemerlamp zelf!
+    //Bericht is van de schemerlamp zelf!
     if (strlen(bericht) == 3)
     {
-        this->beweging = atoi(bericht);
-        moetIkIetsDoen(bericht);
+        this->beweging = atoi(bericht); //Bericht omzetten naar integer en opslaan in de beweging int. this zorgt voor toekomstbestendigheid als er later meer met beweging gedaan wordt.
+        moetIkIetsDoen(bericht); //Ga iets doen adhv de inhoud van het bericht
     }
-    //dit is een update van de potmeter van de muur
+    //Dit is een update van de potmeter van de muur
     if(strlen(bericht) ==8)
     {
         char temp[20];
-        strcpy(temp, bericht);
-        //std::string TempWaardeLed(bericht);
-       // this->waardeLed = std::bitset<8>(TempWaardeLed).to_ulong();
-        //char* temp;
-        //temp[0] = 1;
-        //strcpy(temp + 1, TempWaardeLed.c_str());
-        server->stuurBericht(GeefFD(), temp);
+        strcpy(temp, bericht); //sla een copy bericht op in een tijdelijke buffer
+        server->stuurBericht(GeefFD(), temp); //Verstuur het bericht naar de socket die hoort bij de huidige Schemerlamp om uit te voeren en de status van de lamp bij te werken.
     }
 }
 
-char *Schemerlamp::GeefData()
+char *Schemerlamp::GeefData() //Niet geïmplementeerde functie van het teruggeven van gegevens.
 {
     return nullptr;
 }
 
 void Schemerlamp::moetIkIetsDoen(char *bericht)
 {
-    // kijken of er beweging is
+    // Er is beweging gedetecteerd
     if (beweging == 255)
     {
         std::cout << "Er is beweging, signaal moet nog doorgegeven worden aan de bewaker als dit niet Mary is." << std::endl;
 
-        for (auto it = server->GeefPointerMap().begin(); it != server->GeefPointerMap().end(); it++)
+        for (auto it = server->GeefPointerMap().begin(); it != server->GeefPointerMap().end(); it++) //Voor alle clients in de map, zoek naar Mary.
         {
-            if (it->second->GeefType() == 5)
+            if (it->second->GeefType() == 5) //Voor elke client met het type van Mary
             {
-                Client *client = it->second;
-                Mary *mary = dynamic_cast<Mary*>(client);
-                if(mary->isMaryThuis() == 0)
+                Client *client = it->second; //Creër een pointer naar een Client object uit de base class.
+                Mary *mary = dynamic_cast<Mary*>(client); //Zet pointer via dynamic cast om naar de afgeleide klasse Mary, om toegang te krijgen tot alle functies specifiek van Mary.
+                if(mary->isMaryThuis() == 0) //Vraag op of Mary thuis is, en als dat niet het geval is:
                 {
-                    for(auto its = server->GeefPointerMap().begin(); its != server->GeefPointerMap().end(); its++)
+                    for(auto its = server->GeefPointerMap().begin(); its != server->GeefPointerMap().end(); its++) //Zoek naar de bewaker in de map
                     {
-                        if(its->second->GeefType() == 6)
+                        if(its->second->GeefType() == 6) //Als de bewaker gevonden is:
                         {
-                            server->stuurBericht(its->first, "Inbraak! Beweging in Mary haar appartement!\n" );
+                            server->stuurBericht(its->first, "Inbraak! Beweging in Mary haar appartement!\n"); //Stuur een bericht dat er inbraak is.
                         }
                     }
                 }
-                //server->stuurBericht(it->first, "Er is beweging!");
             }
         }
     }
-    if (beweging == 254)
+    if (beweging == 254) //De beweging is gestopt
     {
         std::cout << "Er is geen beweging meer, signaal moet nog doorgegeven worden aan Mary" << std::endl;
 
-        for (auto it = server->GeefPointerMap().begin(); it != server->GeefPointerMap().end(); it++)
+        for (auto it = server->GeefPointerMap().begin(); it != server->GeefPointerMap().end(); it++) //Zoek naar Mary in de clients map
         {
             if (it->second->GeefType() == 5)
             {
-                Client *client = it->second;
-                Mary *mary = dynamic_cast<Mary*>(client);
-                if(mary->isMaryThuis() == 0)
+                Client *client = it->second; //Creër een pointer naar een Client object uit de base class.
+                Mary *mary = dynamic_cast<Mary*>(client); //Zet pointer via dynamic cast om naar de afgeleide klasse Mary, om toegang te krijgen tot alle functies specifiek van Mary.
+                if(mary->isMaryThuis() == 0) //Vraag op of Mary thuis is, en als dat niet het geval is:
                 {
-                    server->stuurBericht(it->first, "Er is bij je ingebroken! Er is nu geen beweging meer!");
+                    server->stuurBericht(it->first, "Er is bij je ingebroken! Er is nu geen beweging meer!"); //Laat aan Mary weten dat er geen beweging in haar appartement meer is.
                 }
             }
         }
