8024d339fbf6d40420e4baadd1c24c779e554d21
Sander-132 <70809337+Sander-132@users.noreply.github.com>
2025-01-27 11:29:16 +0100

    Update Schemerlamp.h
    Opschonen + comments

--- Schemerlamp.h
+++ Schemerlamp.h
@@ -1,6 +1,6 @@
 #pragma once // Voorkomen van meervoudige inclusie van de header file
 
-#include "Client.h"
+#include "Client.h" 
 
 class Schemerlamp : public Client // Overerven van de Client klasse
 {
@@ -8,12 +8,10 @@ public:
     Schemerlamp(int, int, Server *); // Constructor
     virtual ~Schemerlamp();                  // Destructor
     virtual void Update(char *);     // Functie voor het updaten van de Schemerlamp
-    virtual char *GeefData();        // Functie voor het geven van de data van de Schemerlamp
-    virtual void moetIkIetsDoen(char *bericht);
+    virtual char *GeefData();        // Functie voor het geven van de data van de Schemerlamp, doet niks in .cpp is alleen hier van toepassing voor eventuele toekomstige uitbreiding
+    virtual void moetIkIetsDoen(char *bericht); //Functie voor het uitvoeren van een signaal adhv het bericht
 
-    // Functies nog toevoegen
 private:
     int waardeLed;
     int beweging;
-    // Variabelen nog toevoegen
 };
